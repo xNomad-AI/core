6fdcf2357ba05720b253a1d8d80e27f4e5c2e494
ouzhuohong <smileuwu97@gmail.com>
2025-01-02 10:59:28 +0800

    feat: add plugin-ai-nft

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,15 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'max-len': [
+      "error", 
+      {
+        "code": 120, 
+        "ignoreComments": true,
+        "ignoreUrls": true, 
+        "ignoreStrings": true,
+        "ignoreTemplateLiterals": true 
+      }
+    ],
   },
 };
