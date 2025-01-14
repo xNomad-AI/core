91423a4ecc0c3963bf451d6fcca75bd030e66b7c
everimbaq <everimbaq@163.com>
2025-01-14 17:19:43 +0800

    fix: eliza starter set agent env correctly

--- src/shared/utils.service.ts
+++ src/shared/utils.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class UtilsService {
@@ -13,4 +14,21 @@ export class UtilsService {
   ): input is PromiseRejectedResult {
     return input.status === 'rejected';
   }
+
+  static getEnvFromFile(filepath: string): Record<string, string>{
+    const envFileContent = readFileSync(filepath, 'utf-8');
+    const envVars = envFileContent
+      .split('\n')
+      .filter((line) => line.trim() !== '' && !line.startsWith('#'))
+      .map((line) => {
+        const [key, value] = line.split('=');
+        if (key && value) {
+          return [key.trim(), value.trim()] as [string, string];
+        }
+        return null;
+      })
+      .filter((item): item is [string, string] => item !== null);
+    return Object.fromEntries(envVars);
+  }
+
 }
