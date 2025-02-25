ed84b8dde94657bb435d98322bdd02ccd47a5e48
roger <838845201@qq.com>
2025-02-26 04:34:44 +0800

    feat: copy trade api

--- src/shared/trade-monitor.service.ts
+++ src/shared/trade-monitor.service.ts
@@ -4,13 +4,22 @@ import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 import { TransientLoggerService } from './transient-logger.service.js';
 
-interface TradeMonitorCreateResponse {
+interface LimitOrderCreateResponse {
   id: number;
   tokenAddress: string;
   clientId: string;
   callbackUrl: string;
   conditionType: ConditionType;
   targetPrice: number;
+  status: string;
+  expiredAt: number;
+}
+
+interface CopyTradeCreateResponse {
+  id: number;
+  address: string;
+  clientId: string;
+  callbackUrl: string;
   status: string;
   expiredAt: number;
 }
@@ -30,11 +39,18 @@ interface CreateOrderParams {
   expiredAt: number;
 }
 
+interface CreateCopyTradeParams {
+  targetAddress: string;
+  walletAddress: string;
+  expiredAt: number;
+}
+
 @Injectable()
 export class TradeMonitorService {
   private endpoint: string;
   private apikey: string;
-  private callbackUrl: string;
+  private limitOrderCallbackUrl: string;
+  private copyTradeCallbackUrl: string;
   constructor(
     private readonly logger: TransientLoggerService,
     private readonly appConfig: ConfigService,
@@ -44,12 +60,13 @@ export class TradeMonitorService {
       'TRADE_MONITOR_SERVICE_ENDPOINT',
     )!;
     this.apikey = this.appConfig.get<string>('TRADE_MONITOR_SERVICE_API_KEY')!;
-    this.callbackUrl = `${this.appConfig.get<string>('TRADE_MONITOR_CALLBACK_URL')}/order_callback`; // todo @everimbaq: add order_callback route
+    this.limitOrderCallbackUrl = `${this.appConfig.get<string>('TRADE_MONITOR_CALLBACK_URL')}/limitOrder_callback`; // todo @everimbaq: add limit_order_callback route
+    this.copyTradeCallbackUrl = `${this.appConfig.get<string>('TRADE_MONITOR_CALLBACK_URL')}/copyTrade_callback`; // todo @everimbaq: add copy_trade_callback route
   }
 
   async createLimitOrder(
     params: CreateOrderParams,
-  ): Promise<TradeMonitorCreateResponse> {
+  ): Promise<LimitOrderCreateResponse> {
     try {
       const config = {
         method: 'POST',
@@ -61,11 +78,11 @@ export class TradeMonitorService {
         data: {
           ...params,
           clientId: params.walletAddress,
-          callbackUrl: this.callbackUrl,
+          callbackUrl: this.limitOrderCallbackUrl,
         },
       };
       const response = await firstValueFrom(this.httpService.request(config));
-      const createResponse = response.data as TradeMonitorCreateResponse;
+      const createResponse = response.data as LimitOrderCreateResponse;
       if (!createResponse.id) {
         throw new Error('Failed to create order');
       }
@@ -98,6 +115,61 @@ export class TradeMonitorService {
       return;
     } catch (e) {
       this.logger.error(`Failed to cancel order: ${e}`);
+      throw e;
+    }
+  }
+
+  async createCopyTrade(
+    params: CreateCopyTradeParams,
+  ): Promise<CopyTradeCreateResponse> {
+    try {
+      const config = {
+        method: 'POST',
+        url: `${this.endpoint}/address-monitors`,
+        headers: {
+          'Content-Type': 'application/json',
+          'API-KEY': this.apikey,
+        },
+        data: {
+          ...params,
+          clientId: params.walletAddress,
+          callbackUrl: this.copyTradeCallbackUrl,
+        },
+      };
+      const response = await firstValueFrom(this.httpService.request(config));
+      const createResponse = response.data as CopyTradeCreateResponse;
+      if (!createResponse.id) {
+        throw new Error('Failed to create copy trade');
+      }
+      this.logger.log(
+        `Copy trade created successfully, CopyTradeId: ${createResponse.id}`,
+      );
+
+      return createResponse;
+    } catch (e) {
+      this.logger.error(`Failed to create copy trade: ${e}`);
+      throw e;
+    }
+  }
+
+  async cancelCopyTrade(orderId: number) {
+    try {
+      const config = {
+        method: 'DELETE',
+        url: `${this.endpoint}/address-monitors/${orderId}`,
+        headers: {
+          'Content-Type': 'application/json',
+          'API-KEY': this.apikey,
+        },
+      };
+      const response = await firstValueFrom(this.httpService.request(config));
+      if (response.status !== 200) {
+        throw new Error('Failed to cancel copy trade');
+      }
+
+      return;
+    } catch (e) {
+      this.logger.error(`Failed to cancel copy trade: ${e}`);
       throw e;
     }
   }
