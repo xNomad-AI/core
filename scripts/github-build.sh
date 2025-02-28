505d0a38067f69efbc2ea4edffd16204276cc9e0
Benny <b1623546466@gmail.com>
2025-02-28 16:40:29 +0800

    fix: preinstall1

--- scripts/github-build.sh
+++ scripts/github-build.sh
@@ -7,16 +7,16 @@ VERSION=$1
 TARGET_DIR="data/eliza"
 # check if dir exists
 if [ -d "$TARGET_DIR" ]; then
-  git fetch --all
-  git checkout $VERSION
   echo "exists"
 else
-  git clone -b $VERSION https://github.com/xNomad-AI/eliza.git $TARGET_DIR
+  git clone -b develop https://github.com/xNomad-AI/eliza.git $TARGET_DIR
 fi
 
 cd $TARGET_DIR
-git pull
-pnpm run clean || true
+git fetch --all
+git checkout $VERSION
+
+# pnpm run clean || true
 
 node --version
 pnpm install --no-frozen-lockfile
