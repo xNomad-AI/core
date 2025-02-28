933e842dc8b0c8d965b294b88ca7b4c21186c81b
everimbaq <everimbaq@163.com>
2025-02-28 15:46:32 +0800

    feat: add page query for memories

--- src/eliza/packages/plugin-solana/src/actions/autoSwap.ts
+++ src/eliza/packages/plugin-solana/src/actions/autoSwap.ts
@@ -111,8 +111,25 @@ Respond with a JSON markdown block containing only the extracted values. Use nul
 }
 \`\`\`
 
-Examples: 
-1. Create an automatic task to buy ai16z with 0.0001 SOL when the token price is below $1.
+# Examples: 
+1. Create an automatic task to buy 74SBV4zDXxTRgv1pEMoECskKBkZHc2yGPnc7GYVepump with 0.0001 SOL when the token price is below 0.1
+The response should be
+{
+  "inputTokenSymbol": "SOL",
+  "inputTokenCA": null,
+  "outputTokenCA": "74SBV4zDXxTRgv1pEMoECskKBkZHc2yGPnc7GYVepump",
+  "outputTokenSymbol": null,
+  "inputTokenAmount": 0.0001,
+  "inputTokenPercentage": null,
+  "outputTokenAmount": null,
+  "delay": null,
+  "startAt": null,
+  "expireAt": null,
+  "priceCondition": "below",
+  "priceTarget": 0.1,
+  "tokenTarget": "74SBV4zDXxTRgv1pEMoECskKBkZHc2yGPnc7GYVepump"
+}
+2. Create an automatic task to buy ai16z with 0.0001 SOL when the token price is below $1.
 The response should be 
 {
   "priceTarget": "1"
@@ -127,7 +144,7 @@ The response should be
   "outputTokenSymbol": "ai16z",
   "inputTokenSymbol": "SOL",
 } 
-2. auto sell 1000 ai16z to SOL when the token price is above $1.
+3. auto sell 1000 ai16z to SOL when the token price is above $1.
 The response should be 
 {
   "priceTarget": "1"
@@ -142,7 +159,7 @@ The response should be
   "outputTokenSymbol": "SOL",
   "inputTokenSymbol": "ai16z",
 } 
-3. auto sell 20% of ai16z to SOL when the token price is above $1.
+4. auto sell 20% of ai16z to SOL when the token price is above $1.
 The response should be 
 {
   "priceTarget": "1"
