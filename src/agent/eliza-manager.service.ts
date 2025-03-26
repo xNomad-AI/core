7e07a3bf7feb267a0fdedddc9619d7c94ca08e14
everimbaq <everimbaq@163.com>
2025-03-26 10:18:46 +0800

    chore: update pnpm lock

--- src/agent/eliza-manager.service.ts
+++ src/agent/eliza-manager.service.ts
@@ -369,9 +369,9 @@ export class ElizaManagerService {
     character.settings.secrets['WALLET_SECRET_SALT'] = salt;
     character.settings['WALLET_SECRET_SALT'] = salt;
     character.settings.secrets['NFT_CHAIN'] = chain;
+    character.settings['NFT_CHAIN'] = chain;
     character.settings.secrets['NFT_ID'] = nftId;
-    character.settings['WALLET_SECRET_SALT'] = salt;
-    character.settings['TEE_MODE'] = teeMode;
+    character.settings['NFT_ID'] = nftId;
     character.settings.modelConfig = {
       ...character.settings.modelConfig,
       temperature: 0.2,
