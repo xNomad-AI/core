843f0730f49678cf5096e75f34211c83dc30481b
everimbaq <everimbaq@163.com>
2025-03-27 16:44:44 +0800

    chore: add address field of wallet api

--- src/eliza/packages/plugin-evm/src/providers/walletUtils.ts
+++ src/eliza/packages/plugin-evm/src/providers/walletUtils.ts
@@ -6,6 +6,7 @@ let isMoralisInitialized = false;
 
 export class WalletPortfolio {
   items: Item[];
+  wallet: string;
   totalUsd: number;
   nextCursor?: string;
 }
@@ -118,6 +119,7 @@ export async function getWalletPortfolio(
     });
     const walletPortfolio: WalletPortfolio = {
       items: [],
+      wallet: address,
       totalUsd: 0
     };
     response.response.result.forEach((item) => {
